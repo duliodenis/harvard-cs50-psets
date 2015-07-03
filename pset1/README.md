4c6d116b4a11c4ff9cd465da0460845cab5c82fe
Dulio Denis <dulio.denis@yahoo.com>
2015-07-03 13:48:07 -0400

    Updated PSet 1 mario inverted pyramid with check50 output

---
+++ pset1/README.md
@@ -0,0 +1,19 @@
+# Harvard CS50: Problem Set 1
+This repo contains my solution to [problem set 1](https://github.com/duliodenis/harvard-cs50-psets/blob/master/pset1/pset1.pdf). These solutions are meant as a way to share both my logic and thinking as well as to solicit feedback from members of the community.
+
+### 1. Hello, C
+Warm up exercise that prints out "hello, world"
+
+###2. Itsa Mario
+![](https://raw.githubusercontent.com/duliodenis/harvard-cs50-psets/master/art/mario.png)
+
+A program that recreates this half-pyramid using hashes (#) for blocks.
+
+![](https://raw.githubusercontent.com/duliodenis/harvard-cs50-psets/master/art/mario-inverted-pyramid.png)
+
+###3. Time for Change
+
+An implementation of a greedy algorithm for making change.
+
+### Support or Contact
+Visit [ddApps.co](http://ddapps.co) to see more.
--- pset1/mario.c
+++ pset1/mario.c
@@ -6,18 +6,55 @@
 
 int main(void)
 {
-    printf("height: ");
-    int height = GetInt();
-    // check to see that height is > 0 and < 23
+
+    int height;
+
+    // ensure the height is between 0 and 23
+    do {
+        printf("height: ");
+        height = GetInt();
+        
+        // provide a tip
+        if ((height < 0) || (height > 23)) printf("Ensure the value is between 1 and 23\n");
+    } while ((height < 0) || (height > 23));
+    
+    int width = height + 1; // the width of the pyramid is one more than the height
     
     for (int pyramidHeight = 0; pyramidHeight < height; pyramidHeight++) {
-        int spaces = 0;
-        for (int steps = 0; steps < height + 1; steps++) {
-            if (spaces < pyramidHeight - steps -1) printf(" ");
+
+        for (int pyramidWidth = 0; pyramidWidth < width; pyramidWidth++) {
+        
+            if (pyramidWidth <= width-3-pyramidHeight) printf(" ");
             else printf("#");   
-            spaces++; 
+
         }
         printf("\n");
     }
 }
 
+/* Output Check:
+jharvard@appliance (~/psets): ./mario
+height: 100
+Ensure the value is between 1 and 23
+height: 8
+       ##
+      ###
+     ####
+    #####
+   ######
+  #######
+ ########
+#########
+jharvard@appliance (~/psets): check50 2014.fall.pset1.mario mario.c
+Checking..........................................................................................................:) mario.c exists
+:) mario.c compiles
+:) rejects a height of -1
+:) handles a height of 0 correctly
+:) handles a height of 1 correctly
+:) handles a height of 2 correctly
+:) handles a height of 23 correctly
+:) rejects a height of 24
+:) rejects a non-numeric height of "foo"
+:) rejects a non-numeric height of ""
+https://sandbox.cs50.net/checks/d276bd1496f34ba1becb13aeeb39d30c
+*/
